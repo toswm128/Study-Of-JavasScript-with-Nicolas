2c09243737441e3f3bb42e0f151dcc8fca80741d
toswm128 <chominsu10@gmail.com>
2020-10-17 21:57:44 +0900

    const와 let 정리

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 //let => 변수
 
 //var =>요즘엔 안씀 
-
-console.log('iam javascript');
+const name ="aa";
+let a = 2;
+const b =10;
+/*b = b+1;*/
+a = a +1;
+console.log(a /*,b*/,b+1/*오류는 안뜨지만 더해지지는 않음*/);
