e7dc58f4a84cf0ee23dece3b7ade36afa4b1ee27
toswm128 <chominsu10@gmail.com>
2020-10-18 15:05:17 +0900

    배열

--- index.js
+++ index.js
@@ -2,9 +2,26 @@
 //let => 변수
 
 //var =>요즘엔 안씀 
-const name ="aa";
+/*
+st name ="aa";
 let a = 2;
 const b =10;
-/*b = b+1;*/
+//b = b+1;
 a = a +1;
-console.log(a /*,b*/,b+1/*오류는 안뜨지만 더해지지는 않음*/);
+*/
+//console.log(a /*,b*/,b+1/*오류는 안뜨지만 더해지지는 않음*/);
+const boolean = true;//boolean
+console.log(boolean);
+const float = 51.1;
+console.log(float)
+//const daysOfWeek 변수를 작성할때 첫번째는 소문자 뛰어쓰기는 대문자로!
+const mon = "Mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+console.log(mon,thu,wed,thu,fri);
+//array 안쓸경우
+const daysOfWeek = ["Mon","Tue","Wed","Thu","Fri",
+"Sat","Sun"];
+console.log(daysOfWeek[2]);
